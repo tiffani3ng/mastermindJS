ae6253497232b6ab3676387337c712a711bd6314
tiffani3ng <157239316+tiffani3ng@users.noreply.github.com>
2024-03-25 21:18:56 -0400

    Update mmScript.js

--- mmScript.js
+++ mmScript.js
@@ -1,56 +1,70 @@
 class Code {
   constructor() {
-    this.code = [];
-  }
+    this.code = []; // constructor, creates an empty array
+  } // end constructor
 
   genCode() {
     this.code = [];
     for (let i = 0; i < 4; i++) {
       this.code.push(Math.floor(Math.random() * 7) + 1);
+      // generates a random 4-digit code and stores it in the array
+      // of the code object
     }
-  }
+  } // end genCode
 
   getCode() {
-    return this.code;
-  }
-}
+    return this.code; // accessor for the code
+  } // end getCode
+} // end Code class
 
 class Board {
+
   constructor(code) {
-    this.currentGuess = [];
-    this.circles = [];
-    this.currentGuessNum = 1;
-    this.correctCode = code;
-    this.playerScore = 0;
+    // constructor takes code key as a parameter
+    // sets all variables to default empty values
+    this.currentGuess = []; // array to store user guess
+    this.circles = []; // array to store circles choices
+    this.currentGuessNum = 1; // guess number
+    this.correctCode = code; // array to store code key
+    this.playerScore = 0; // setting scores to zero
     this.compScore = 0;
-  }
+  } // end constructor
 
   resetBoard() {
-    const guessCircles = document.querySelectorAll('.circle');
+    const guessCircles = document.querySelectorAll('.circle'); // selecting all circles
     guessCircles.forEach(circle => {
-      circle.style.backgroundColor = '';
-      circle.style.border = '2px solid #000';
+      circle.style.backgroundColor = '#f8f8f8'; // reset colors
+      circle.style.border = '2px solid #000'; // reset hint borders
     });
+    // resetting values that store user input
     this.currentGuess = [];
     this.currentGuessNum = 1;
     this.circles = [];
+    // resetting scores
     this.playerScore = 0;
     this.compScore = 0;
-  }
+  } // end resetBoard
 
-  guess(number, currentColor) {
+  guess(number) {
+    // guess function takes number as a parameter
     if ((Math.floor((number - 1) / 4) + 1) == this.currentGuessNum) {
+      // if the circle is in a valid row
       if (!this.circles.includes(number)) {
+        // pushes the circle number into the circles array
         this.circles.push(number);
       }
-      if (currentColor !== null && number !== undefined) {
-        document.getElementById('row' + (Math.floor((number - 1) / 4) + 1)).children[(number - 1) % 4].style.backgroundColor = currentColor;
-        this.currentGuess[(number - 1) % 4] = this.getColorIndex(currentColor);
+      if (this.currentColor !== null && number !== undefined) {
+        // if a color is selected and there is no error
+        document.getElementById('row' + (Math.floor((number - 1) / 4) + 1)).children[(number - 1) % 4].style.backgroundColor = this.currentColor;
+        // change the color of the circle to the color selected
+        this.currentGuess[(number - 1) % 4] = this.getColorIndex(this.currentColor);
+        // add the guess to the guess array
       }
     }
-  }
+  } // end guess
 
   getColorIndex(color) {
+    // converting color hex codes to a number
     switch (color) {
       case '#FFADAD':
         return 1;
@@ -71,81 +85,105 @@ class Board {
       default:
         return -1;
     }
-  }
+  } // end getColorIndex
 
   checkGuess() {
     if (this.currentGuess.length === 4) {
+      // if the guess is a complete guess
       const hint = this.getHint(this.currentGuess);
+      // get a hint code array
       this.circles.sort((a, b) => a - b);
+      // sorts the circles array in case user chose out of order
       for (let i = 0; i < 4; i++) {
+        // for each code in array
         const hintElement = document.getElementById('hint' + this.circles[i].toString());
+        // get hint element to change color
         if (hint[i] === 1) {
+          // if right color, right position, change to green
           hintElement.style.border = '3px solid #08c017';
         } else if (hint[i] === 2) {
+          // if right color, wrong position, change to yellow
           hintElement.style.border = '3px solid #FFEE00';
         } else if (hint[i] === 0) {
+          // if wrong color, change to gray
           hintElement.style.border = '3px solid gray';
         }
       }
       if (hint.every(val => val === 1)) {
+        // winning message if every color is correct
         alert('Congratulations! You won!');
+        // increment player score
         this.playerScore += 1;
+        // reflect new score on screen
         document.getElementById('scoreP').innerText = this.playerScore.toString();
-        this.resetBoard();
       } else if (this.currentGuessNum === 10) {
+        // losing message if you ran out of guesses
         alert('You ran out of guesses. The computer wins!');
+        // increment computer score
         this.compScore += 1;
+        // reflect new score on screen
         document.getElementById('scoreC').innerText = this.compScore.toString();
-        this.resetBoard();
       }
+      // reset variables for user input for new guesses
       this.currentGuess = [];
       this.circles = [];
-      currentColor = null;
+      this.currentColor = null;
+      // increment guess number
       this.currentGuessNum += 1;
     } else {
+      // error message if guess is incomplete
       alert('Please fill all circles before checking.');
     }
-  }
+  } // end checkGuess
 
   getHint(guess) {
     const hint = [];
+    // empty hint array
     for (let i = 0; i < guess.length; i++) {
       if (guess[i] === this.correctCode[i]) {
+        // if right color, right position
         hint.push(1);
       } else if (this.correctCode.includes(guess[i])) {
+        // if right color, wrong place
         hint.push(2);
       } else {
+        // if wrong color
         hint.push(0);
       }
     }
-    return hint;
-  }
+    return hint; // return hint key array
+  } // end getHint
 
   selectColor(color) {
-    currentColor = color;
-  }
+    this.currentColor = color; // sets currentColor
+  } // end selectColor
 
-}
+} // end Board class
 
 class MastermindGame {
   constructor() {
-    this.code = new Code();
-    this.code.genCode();
-    this.board = new Board(this.code.getCode());
-  }
+    // constructor sets variables to default values
+    this.code = new Code(); // new code object to hold code key
+    this.code.genCode(); // generates a new code
+    this.board = new Board(this.code.getCode()); // creates new board for game
+  } // end constructor
 
   instructions() {
+    // function to print instructions
     alert('The computer generates a secret code of 4 colors. The player has 10 attempts to guess the code before losing. \n\nChecking the guess tells the player if each peg is either \n\n1) the right color in the right slot [green], \n2) the right color in the wrong slot [yellow], or \n3) the wrong color entirely [gray]. \n\nSelecting a new round resets the pegboard and generates a new code, and resetting the game also resets the scoreboard.');
-  }
+  } // end instructions
 
   resetGame() {
-    this.board.resetBoard();
+    this.board.resetBoard(); // reset board
+    // set scores to zero
     this.board.playerScore = 0;
     this.board.compScore = 0;
+    // reflect score change on screen
     document.getElementById('scoreP').innerText = 0;
     document.getElementById('scoreC').innerText = 0;
+    // generate new code
     this.code.genCode();
-  }
+  } // end resetgame
 }
 
-const game = new MastermindGame();
+const game = new MastermindGame(); // create new game
