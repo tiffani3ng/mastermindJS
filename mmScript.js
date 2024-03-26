d37d5d0c8ff37d903cf0f1dc76c932102dc57007
tiffani3ng <157239316+tiffani3ng@users.noreply.github.com>
2024-03-26 13:55:24 -0400

    Update mmScript.js

--- mmScript.js
+++ mmScript.js
@@ -5,8 +5,8 @@ class Code {
 
   genCode() {
     this.code = [];
-    for (let i = 0; i < 4; i++) {
-      this.code.push(Math.floor(Math.random() * 7) + 1);
+    for (var i = 0; i < 4; i++) {
+      this.code.push(Math.floor(Math.random() * 8) + 1);
       // generates a random 4-digit code and stores it in the array
       // of the code object
     }
@@ -15,38 +15,48 @@ class Code {
   getCode() {
     return this.code; // accessor for the code
   } // end getCode
+
+  addToCode(index, value) {
+    this.code[index] = value;
+  } // end addToCode
+
+  clearCode() {
+    this.code = [];
+  } // end clearCode
+
+  compareCode(userCode) {
+    // function to compare the values of this code object to another code object
+    var hint = [];
+    // empty hint array
+    for (var i = 0; i < userCode.getCode().length; i++) {
+      if (userCode.getCode()[i] === this.code[i]) {
+        // if right color, right position
+        hint.push(1);
+      } else if (this.code.includes(userCode.getCode()[i])) {
+        // if right color, wrong place
+        hint.push(2);
+      } else {
+        // if wrong color
+        hint.push(0);
+      }
+    }
+    return hint; // return hint key array
+  } // end compareCode
+
 } // end Code class
 
 class Board {
 
   constructor() {
     // sets all variables to default empty values
-    this.currentGuess = []; // array to store user guess
+    this.currentGuess = new Code(); // code object to store user guess
     this.circles = []; // array to store circles choices
     this.currentGuessNum = 1; // guess number
-    this.code = new Code(); // new code object to hold code key
-    this.code.genCode(); // generates a new code
-    this.correctCode = this.code.getCode(); // array to store code key
+    this.codeKey = new Code(); // new code object to hold code key
+    this.codeKey.genCode(); // generates a new code for code key
     this.playerScore = 0; // setting scores to zero
     this.compScore = 0;
   } // end constructor
-
-  resetBoard() {
-    const guessCircles = document.querySelectorAll('.circle'); // selecting all circles
-    guessCircles.forEach(circle => {
-      circle.style.backgroundColor = '#f8f8f8'; // reset colors
-      circle.style.border = '2px solid #000'; // reset hint borders
-    });
-    // resetting values that store user input
-    this.currentGuess = [];
-    this.currentGuessNum = 1;
-    this.circles = [];
-    // resetting scores
-    this.playerScore = 0;
-    this.compScore = 0;
-    // choosing a new code
-    this.correctCode.
-  } // end resetBoard
 
   guess(number) {
     // guess function takes number as a parameter
@@ -60,8 +70,8 @@ class Board {
         // if a color is selected and there is no error
         document.getElementById('row' + (Math.floor((number - 1) / 4) + 1)).children[(number - 1) % 4].style.backgroundColor = this.currentColor;
         // change the color of the circle to the color selected
-        this.currentGuess[(number - 1) % 4] = this.getColorIndex(this.currentColor);
-        // add the guess to the guess array
+        this.currentGuess.addToCode((number - 1) % 4, this.getColorIndex(this.currentColor));
+        // add the guess to the guess code object
       }
     }
   } // end guess
@@ -91,15 +101,15 @@ class Board {
   } // end getColorIndex
 
   checkGuess() {
-    if (this.currentGuess.length === 4) {
+    if (this.currentGuess.getCode().length === 4) {
       // if the guess is a complete guess
-      const hint = this.getHint(this.currentGuess);
+      var hint = this.codeKey.compareCode(this.currentGuess);
       // get a hint code array
       this.circles.sort((a, b) => a - b);
       // sorts the circles array in case user chose out of order
-      for (let i = 0; i < 4; i++) {
-        // for each code in array
-        const hintElement = document.getElementById('hint' + this.circles[i].toString());
+      for (var i = 0; i < 4; i++) {
+        // for each hint in array
+        var hintElement = document.getElementById('hint' + this.circles[i].toString());
         // get hint element to change color
         if (hint[i] === 1) {
           // if right color, right position, change to green
@@ -128,7 +138,7 @@ class Board {
         document.getElementById('scoreC').innerText = this.compScore.toString();
       }
       // reset variables for user input for new guesses
-      this.currentGuess = [];
+      this.currentGuess.clearCode();
       this.circles = [];
       this.currentColor = null;
       // increment guess number
@@ -139,35 +149,28 @@ class Board {
     }
   } // end checkGuess
 
-  getHint(guess) {
-    const hint = [];
-    // empty hint array
-    for (let i = 0; i < guess.length; i++) {
-      if (guess[i] === this.correctCode[i]) {
-        // if right color, right position
-        hint.push(1);
-      } else if (this.correctCode.includes(guess[i])) {
-        // if right color, wrong place
-        hint.push(2);
-      } else {
-        // if wrong color
-        hint.push(0);
-      }
-    }
-    return hint; // return hint key array
-  } // end getHint
-
   selectColor(color) {
     this.currentColor = color; // sets currentColor
   } // end selectColor
+
+  resetBoard() {
+    var guessCircles = document.querySelectorAll('.circle'); // selecting all circles
+    guessCircles.forEach(circle => {
+      circle.style.backgroundColor = '#f8f8f8'; // reset colors
+      circle.style.border = '2px solid #000'; // reset hint borders
+    });
+    // resetting values that store user input
+    this.currentGuess.clearCode();
+    this.currentGuessNum = 1;
+    this.circles = [];
+    // choosing a new code
+    this.codeKey.genCode();
+  } // end resetBoard
 
 } // end Board class
 
 class MastermindGame {
   constructor() {
-    // constructor sets variables to default values
-    this.code = new Code(); // new code object to hold code key
-    this.code.genCode(); // generates a new code
     this.board = new Board(); // creates new board for game
   } // end constructor
 
@@ -185,6 +188,15 @@ class MastermindGame {
     document.getElementById('scoreP').innerText = 0;
     document.getElementById('scoreC').innerText = 0;
   } // end resetgame
+
+  checkGuess() {
+    this.board.checkGuess();
+  } // end checkGuess
+
+  newRound() {
+    this.board.resetBoard();
+  }
+
 }
 
-const game = new MastermindGame(); // create new game
+var game = new MastermindGame(); // create new game
